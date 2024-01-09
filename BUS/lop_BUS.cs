fee63a2a113993737c1015261f61b511cd32ff9b
MauHieu <mauhieu198@gmail.com>
2024-01-10 02:13:12 +0700

    Add project files.

---
+++ BUS/lop_BUS.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Security.Policy;
+using System.Text;
+using System.Threading.Tasks;
+using DAL;
+using DTO;
+
+namespace BUS
+{
+    public class lop_BUS
+    {
+        lop_DAL phieuthu = new lop_DAL();
+
+        public DataTable Load_BUS()
+        {
+            return phieuthu.Load_Lop();
+        }
+        public void Insert_Bus(phieuthu ob)
+        {
+
+            phieuthu.Insert_Lop(ob);
+        }
+
+        public void Update_Bus(phieuthu ob)
+        {
+            phieuthu.Update_Lop(ob);
+        }
+        public void Delete_Bus(string makh)
+        {
+            phieuthu.Delete_Lop(makh);
+        }
+
+    }
+}
---
+++ DAL/lop_DAL.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Security.Policy;
+using System.Text;
+using System.Threading.Tasks;
+using DTO;
+namespace DAL
+{
+    public class lop_DAL:Connect
+    {
+        public lop_DAL() { }
+        public DataTable Load_Lop()
+        {
+            return Load_DL("select * from phieuthu");
+        }
+        public void Insert_Lop(phieuthu ob)
+        {
+            string sql = "insert into phieuthu values('" +
+    ob.sopt + "','" + ob.ngaythu + "','" + ob.makh + "','" + ob.sotien + "')";
+            Excecute(sql);
+        }
+        public void Update_Lop(phieuthu ob)
+        {
+            string sql = "update phieuthu set sopt= N'" + ob.sopt + "', sotien = '" + ob.sotien + "'" +
+    "where makh='" + ob.makh + "'";
+            Excecute(sql);
+        }
+        public void Delete_Lop(string makh)
+        {
+            string sql = "delete from phieuthu where makh='" + makh + "'";
+            Excecute(sql);
+        }
+
+    }
+}
